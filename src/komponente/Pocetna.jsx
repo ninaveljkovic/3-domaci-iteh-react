e0bcfd8c90e252e3426cffdf7282d0b5e7ab7149
ninaveljkovic <ninaveljkovic999@gmail.com>
2022-08-27 12:49:32 +0200

    dodat sadrzaj na pocetnu stranicu

---
+++ src/komponente/Pocetna.jsx
@@ -0,0 +1,36 @@
+ 
+function Pocetna() {
+  return (
+    <div  className='pocetna'>
+
+       
+            
+                
+    <div className="hero-text">
+        <h1  > Frizerski salon SrediMe</h1>
+
+    
+    </div>
+  
+    <br /><br /><br /><br /><br />
+    <div className="flex-container">
+        <div>
+            <h2>NEGA KOSE</h2>
+            <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore pariatur quibusdam aperiam natus minima nam,   reprehenderit debitis at non!</p>
+        </div>
+        <div  >          
+            <h2>ZENSKE FRIZURE</h2>
+            <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore pariatur quibusdam aperiam natus minima nam</p>
+        </div>
+        <div >         
+            <h2>MUSKE FRIZURE</h2> 
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore pariatur quibusdam aperiam natus minima nam</p>
+        </div>  
+
+  </div>
+
+</div>
+  );
+}
+
+export default Pocetna;
